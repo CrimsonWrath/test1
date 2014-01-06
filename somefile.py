a7449ed9c4f5d6f5ca65c34842208fd997ab46ad
zachariahgriffith <Zachariah_Griffith@Zachariah-Griffiths-MacBook-Pro.local>
2014-01-05 21:40:02 -0800

    ballin

--- somefile.py
+++ somefile.py
@@ -5,6 +5,11 @@ def test():
 	print('I dont update, I upgreyyed')
 =======
 	print('I update the update yo - CooWiz')
+
+	a = 2
+	b = 5
+	c = a + b
+	print(c)
 	
 test()
 >>>>>>> 26d5e30a98827ad927913bf3c5ae23e801d1d1d4
