0472eed12a406da15b79fc0dfc0dbb9bf304cebf
zachariahgriffith <Zachariah_Griffith@Zachariah-Griffiths-MacBook-Pro.local>
2014-01-05 21:41:47 -0800

    ballin

--- somefile.py
+++ somefile.py
@@ -1,9 +1,7 @@
 def test():
 	print('hi')
 	print('howdy partner')
-<<<<<<< HEAD
 	print('I dont update, I upgreyyed')
-=======
 	print('I update the update yo - CooWiz')
 
 	a = 2
@@ -12,4 +10,3 @@ def test():
 	print(c)
 	
 test()
->>>>>>> 26d5e30a98827ad927913bf3c5ae23e801d1d1d4
